b7e4e80884e1b8f9da257b8f61163355e4c9e42a
David Künnen <dk@brainlap.com>
2018-11-29 20:37:07 +0100

    change lib

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-// this should be the entry point to your library
-module.exports = {
-  demo: require('./demo').default
-};
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
+import styles from './styles.css'
+
+export default class ExampleComponent extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  }
+
+  render() {
+    const {
+      text
+    } = this.props
+
+    return (
+      <div className={styles.test}>
+        Example Component: {text}
+      </div>
+    )
+  }
+}
